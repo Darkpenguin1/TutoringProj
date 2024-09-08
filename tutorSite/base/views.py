a6a7205f6033ff496fe5c22782cb0c7f347095ac
Darkpenguin1 <Frank.E.Peraza@outlook.com>
2024-09-08 19:38:49 -0400

    Database Section

--- tutorSite/base/views.py
+++ tutorSite/base/views.py
@@ -1,25 +1,29 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from .models import Room
 
-rooms = [
-    {'id': 1, 'name': "Lets learn Python"},
-    {'id': 2, 'name': "Lets learn Design"},
-    {'id': 3, 'name': "Lets learn Web Development"},
-]
+
+# how to query db
+# queryset = modelname.objects.all()
+# Var that stores the query
+# modelname
+# model objects attribute
+# method, ex: get(), filter(), exclude()
+
+# rooms = [
+#     {'id': 1, 'name': "Lets learn Python"},
+#     {'id': 2, 'name': "Lets learn Design"},
+#     {'id': 3, 'name': "Lets learn Web Development"},
+# ]
 
 def home(request):
+    rooms = Room.objects.all()
     context = {'rooms':rooms}
     return render(request, "base/home.html", context)   # Specify the app and its templates folder basically blueprints in flask
 
-def room(request, pk):
-    room = None
-
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
-    context = {'room': room}
-
+def room(request, pk):  # primary key 
+    room = Room.objects.get(id=pk)
+    context = {"room": room}
     
     return render(request, "base/room.html", context)
