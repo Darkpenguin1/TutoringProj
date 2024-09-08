7fb7a399b11d76d904890eacb192e46c48b9fdad
Darkpenguin1 <Frank.E.Peraza@outlook.com>
2024-09-07 22:15:13 -0400

    Templates Section

--- tutorSite/base/views.py
+++ tutorSite/base/views.py
@@ -3,5 +3,23 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+rooms = [
+    {'id': 1, 'name': "Lets learn Python"},
+    {'id': 2, 'name': "Lets learn Design"},
+    {'id': 3, 'name': "Lets learn Web Development"},
+]
+
 def home(request):
-    return HttpResponse("Home Page")
+    context = {'rooms':rooms}
+    return render(request, "base/home.html", context)   # Specify the app and its templates folder basically blueprints in flask
+
+def room(request, pk):
+    room = None
+
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+
+    
+    return render(request, "base/room.html", context)
