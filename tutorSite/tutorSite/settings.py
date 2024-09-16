e25d91fac42678982133775f652f092e7bcabff6
Darkpenguin1 <Frank.E.Peraza@outlook.com>
2024-09-16 19:15:33 -0400

    Static Files Complete

--- tutorSite/tutorSite/settings.py
+++ tutorSite/tutorSite/settings.py
@@ -118,7 +118,15 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
+# STATIC_ROOT = [
+#]
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
