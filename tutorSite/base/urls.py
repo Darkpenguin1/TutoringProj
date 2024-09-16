838c0e7b32082edc7cbc9fd6d812d4fea155b0b5
Darkpenguin1 <Frank.E.Peraza@outlook.com>
2024-09-16 16:14:55 -0400

    User Registration Section

--- tutorSite/base/urls.py
+++ tutorSite/base/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 urlpatterns = [
     path('login/', views.loginPage, name="login"),
+    path('register/', views.registerPage, name="register"),
+
     path('logout/', views.logoutUser, name="logout"),
 
     path('', views.home, name="home"),  ## Triggers the function passed into it
