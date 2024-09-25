da4fb64dc4ab95d37c6229f4303b0e4b88ed40d4
Darkpenguin1 <Frank.E.Peraza@outlook.com>
2024-09-25 19:55:32 -0400

    Mobile Responsiveness

--- tutorSite/base/views.py
+++ tutorSite/base/views.py
@@ -76,7 +76,7 @@ def home(request):
         Q(description__icontains=q) 
         ) # while typing if the typed string atleast contains certain amount of letters for ex py
 
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
 
@@ -185,3 +185,12 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
 
     return render(request, 'base/update-user.html', {'form':form})
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics':topics})
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages':room_messages})
